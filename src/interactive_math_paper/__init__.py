5e03f49fcd3cefb82b218478a34e3a31a9ae828a
hannovonbergen <hannovonbergen98@gmail.com>
2025-09-08 11:56:03 +0200

    first version of documentation

--- src/interactive_math_paper/__init__.py
+++ src/interactive_math_paper/__init__.py
@@ -1,3 +1,29 @@
-from .cli import main_cli
+"""
+.. include:: ../../README.md
+"""
 
-__all__ = ["main_cli"]
+from .cli import main_cli
+from . import visitors
+from .conversion import (
+    lex_tex_source,
+    HtmlNode,
+    TexContext,
+    TexVisitor,
+    VisitResult,
+    TexReader,
+    convert,
+    ErrorVisitor,
+)
+
+__all__ = [
+    "main_cli",
+    "visitors",
+    "lex_tex_source",
+    "HtmlNode",
+    "TexContext",
+    "TexVisitor",
+    "VisitResult",
+    "TexReader",
+    "convert",
+    "ErrorVisitor",
+]
